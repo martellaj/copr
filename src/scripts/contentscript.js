c9ea38bdee09f203570b38ddb1919341d0891686
Joe Martella <martellaj@live.com>
2018-01-27 15:23:30 -0800

    Gets description box filled out with work item info

--- src/scripts/contentscript.js
+++ src/scripts/contentscript.js
@@ -1,34 +1,74 @@
-import ext from "./utils/ext";
+(function() {
+    const intervalId = setInterval(() => {
+        const descriptionContainer = document.getElementsByClassName('vc-pullRequestCreate-description-container')[0];
 
-var extractTags = () => {
-  var url = document.location.href;
-  if(!url || !url.match(/^http/)) return;
+        if (descriptionContainer) {
+            // Add element for user to interact with.
+            addAnchorToDom(descriptionContainer);
 
-  var data = {
-    title: "",
-    description: "",
-    url: document.location.href
-  }
+            // Clear interval because we only need this to run once.
+            window.clearInterval(intervalId);
+        }
+    }, 500);
 
-  var ogTitle = document.querySelector("meta[property='og:title']");
-  if(ogTitle) {
-    data.title = ogTitle.getAttribute("content")
-  } else {
-    data.title = document.title
-  }
+    /**
+     * Adds element for user to interact with to the DOM.
+     */
+    const addAnchorToDom = (descriptionContainer) => {
+        const anchor = document.createElement('a');
 
-  var descriptionTag = document.querySelector("meta[property='og:description']") || document.querySelector("meta[name='description']")
-  if(descriptionTag) {
-    data.description = descriptionTag.getAttribute("content")
-  }
+        anchor.innerText = 'Prepare PR';
+        anchor.onclick = onClick;
 
-  return data;
-}
+        descriptionContainer.insertAdjacentElement('afterbegin', anchor);
+    };
 
-function onRequest(request, sender, sendResponse) {
-  if (request.action === 'process-page') {
-    sendResponse(extractTags())
-  }
-}
+    /**
+     * Handles clicks on the programmtically inserted element (i.e. does
+     * all the work).
+     */
+    const onClick = () => {
+        const container = document.getElementsByClassName('vc-pullRequestCreate-branches-container')[0];
+        const branchName = container.getElementsByClassName('selected-item-text')[0].innerText;
 
-ext.runtime.onMessage.addListener(onRequest);
+        // Get work item number.
+        const workItemNumber = getWorkItemNumber(branchName);
+
+        fillDescription(workItemNumber);
+    };
+
+    /**
+     * Given a string name with parts separated by "-" (i.e. joem/foo-123),
+     * this function returns the work item number (i.e. 123).
+     * @param {string} branchName
+     */
+    const getWorkItemNumber = (branchName) => {
+        const branchParts = branchName.split('-');
+
+        for (let part of branchParts) {
+            const converted = Number(part);
+
+            if (!isNaN(converted)) {
+                return converted;
+            }
+        }
+    };
+
+    /**
+     * Fills description box with work item link, deploy URL, and placeholder
+     * for changelist.
+     * @param {number} workItemNumber
+     */
+    const fillDescription = (workItemNumber) => {
+        const descriptionBox = document.getElementById('description-edit');
+
+        let description = '**Links**\n';
+        description += `- #${workItemNumber}\n`;
+        description += '- [Deploy URL]()\n\n'
+        description += '**Changes**\n';
+        description += '- ';
+
+        descriptionBox.value = description;
+        descriptionBox.focus();
+    };
+})();
